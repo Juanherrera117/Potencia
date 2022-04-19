b0ca727ee90b89cbcd153d0642303eb80cde176e
Juanherrera117 <relujuca@gmail.com>
2022-04-19 08:32:39 -0500

    Desarrollo de los ejercicios de potencias

---
+++ potencia.py
@@ -0,0 +1,20 @@
+"""Programa para Calcular la potencia x^y"""
+
+print("--------------------------")
+print("----- POTENCIA x ^ y -----")
+print("--------------------------")
+
+#input 
+x = input("Digite el valor de x: ")
+x = int (x)
+y = input("Digite el valor de y: ")
+y = int (y)
+
+#procesing
+z = x**y
+
+#output
+print(str(x) + " Elevado a la " + str(y) + " es igual a " +str(z))
+
+
+
